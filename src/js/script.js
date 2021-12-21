6d1a60ca1962bd442b6ca68eedb358180f22a389
xdiegoemmax@outlook.com <xdiegoemmax@outlook.com>
2021-12-21 00:04:58 -0600

    Sites mockups and gallery script added

---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let index=0;
+  
+  let webimg = document.getElementById('website')
+  let webtext = document.getElementById('webtext')
+  
+
+  webimg.src = '/images/website1.png'
+  webtext.innerHTML = "<b>BlogDeCafe</b> is a blog dedicated to coffee where tips, recipes and courses are provided."
+
+
+  let images = [
+    '/images/website1.png',
+    '/images/website2.png',
+    '/images/website3.png',
+    '/images/website4.png',
+    '/images/website5.png',
+  ]
+
+  let title = [
+    "BlogDeCafe",
+    "To Do List",
+    "Universidad",
+    "Buscador de autos",
+    "Check in/out"
+  ]
+
+  let text = [
+    "is a blog dedicated to coffee where tips, recipes and courses are provided.",
+    "is a task manager that uses browser storage. Tasks can be added and deleted.",
+    "is a site with courses that can be added to a shopping cart. Items in the cart can be removed. Uses browser storage.",
+    "engine has a list of cars, which can be filtered by make, year, etc.",
+    "is a system designed for a company that wants to check in and check out employees."
+  ]
+
+  const btnNext = document.getElementById('next')
+  btnNext.onclick = function next() {
+    index+=1
+    if (index > images.length - 1 && index > title.length - 1 && index > text.length - 1) {
+      index = 0
+    }
+    webimg.src = images[index]
+    webtext.innerHTML = `<b>${title[index]}</b> ${text[index]}`
+  }
+  
+  const btnPrev = document.getElementById('prev')
+  btnPrev.onclick = function prev() {
+    index-=1
+    if (index < 0) {
+      index = images.length -1
+    }
+    webimg.src = images[index]
+    webtext.innerHTML = `<b>${title[index]}</b> ${text[index]}`
+  }
+
+})
